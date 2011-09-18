8651af2bef5ba74e183ef1c794793040246cee58
Stephen Mann <thanthese@gmail.com>
2011-09-18 15:19:13 -0500

    game loop in place

--- src/freecell/core.clj
+++ src/freecell/core.clj
@@ -2,16 +2,37 @@
   (:require [freecell.definitions :as defs])
   (:require [freecell.display :as disp])
   (:require [freecell.annotations :as anno])
+  (:require [freecell.move :as move])
   (:gen-class))
 
+(def help-msg
+  "This is Freecell, the best solitaire evar!
+
+  To move, enter from-to coordinate then <Enter>. Codes:
+  - a-f :  cascades 1-4
+  - j-; :  cascades 5-8
+  - q-r :  freecells
+  - u   :  all foundations
+
+    For Example, aq<Enter> would move the top card from the first cascade into
+    the first freecell position.  You can enter multiple moves at once:
+    aqswde<Enter>
+
+  Q : quit
+  ? : this help
+  ")
+
 (defn -main [& args]
-  (let [deck (shuffle (defs/deck))]
-    (-> deck
-      defs/board
-      anno/calculate-annotations
-      disp/board)))
-
-;;; temp testing
-
-(def sample-board (let [deck (shuffle (defs/deck))]
-    (-> deck defs/board)))
+  (loop [board (defs/board (shuffle (defs/deck)))]
+    (disp/board (anno/calculate-annotations board))
+    (println "Enter move (? for help):")
+    (let [user-command (read-line)]
+      (cond (re-find #"\?" user-command) (do (println help-msg) (recur board))
+            (re-find #"Q" user-command) "Have a nice day."
+            :else (recur
+                    (reduce (fn [acc-board [from-code to-code]]
+                              (move/move acc-board
+                                         (str from-code)
+                                         (str to-code)))
+                            board
+                            (partition 2 (.replace user-command " " ""))))))))
